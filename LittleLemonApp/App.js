d7268faa3ba2581d5d72807b136b20fbe2463765
James Michael E. Paz <105730089+JmePaz@users.noreply.github.com>
2023-09-05 13:54:14 +0800

    Add navigation screens

--- LittleLemonApp/App.js
+++ LittleLemonApp/App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Onboarding from './screens/Onboarding';
+import Profile from './screens/Profile';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState } from 'react';
 
-
+const Stack = createNativeStackNavigator()
 export default function App() {
+  const [isUserSigned, setUserSigned] = useState(false)
+
+  
+
   return (
-    <Onboarding></Onboarding>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName=''>
+        <Stack.Screen name="OnBoarding" component={<Onboarding/>}/>
+        <Stack.Screen name="Profile" component={<Profile/>}/>
+        <Stack.Screen name="Home" component={<Home/>}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
